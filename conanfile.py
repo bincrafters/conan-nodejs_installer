ceff5212b024e382663d9ea689c8b146cd9c959f
Uilian Ries <uilianries@gmail.com>
2019-01-06 22:21:51 -0200

    Exclude Linux x86
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@
 import os
 import shutil
 from conans import ConanFile, AutoToolsBuildEnvironment, tools
-from conans.errors import ConanException
+from conans.errors import ConanInvalidConfiguration
 
 
 class NodejsInstallerConan(ConanFile):
@@ -18,56 +18,35 @@ class NodejsInstallerConan(ConanFile):
     exports = ["LICENSE.md"]
     exports_sources = "nodejs.patch"
     settings = "os_build", "arch_build"
-    _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
-    _autotools = None
+
+    def configure(self):
+        if arch = self.settings.arch_build == "x86" and self.settings.os_build == "Linux":
+            raise ConanInvalidConfiguration("Linux x86 is not support by nodejs 10.15.0")
 
     def source(self):
-        if self.settings.os_build == "Linux" and self.settings.arch_build == "x86":
-            tools.get("https://nodejs.org/dist/v10.15.0/node-v10.15.0.tar.gz")
-            os.rename("node-v10.15.0", self._source_subfolder)
+        arch = "x64" if self.settings.arch_build == "x86_64" else "x86"
+        if tools.os_info.is_windows:
+            platform = "win"
+            extension = "zip"
+        elif tools.os_info.is_macos:
+            platform = "darwin"
+            extension = "tar.gz"
+        elif tools.os_info.is_linux:
+            platform = "linux"
+            extension = "tar.xz"
         else:
-            arch = "x64" if self.settings.arch_build == "x86_64" else "x86"
-            if tools.os_info.is_windows:
-                platform = "win"
-                extension = "zip"
-            elif tools.os_info.is_macos:
-                platform = "darwin"
-                extension = "tar.gz"
-            elif tools.os_info.is_linux:
-                platform = "linux"
-                extension = "tar.xz"
-            else:
-                raise ConanException("actual os is not supported")
+            raise ConanInvalidConfiguration("Actual OS is not supported.")
 
-            filename = "node-v{}-{}-{}".format(self.version, platform, arch)
-            source_url = "{}/dist/v{}/{}.{}".format(self.homepage, self.version, filename, extension)
-            tools.get(source_url)
-            extracted_dir = filename
-            os.rename(extracted_dir, self._build_subfolder)
-
-    def _configure_autotools(self):
-        if not self._autotools:
-            self._autotools = AutoToolsBuildEnvironment(self)
-            self._autotools.configure()
-        return self._autotools
-
-    def build(self):
-        if self.settings.os_build == "Linux" and self.settings.arch_build == "x86":
-            with tools.chdir(self._source_subfolder):
-                autotools = self._configure_autotools()
-                autotools.make()
+        filename = "node-v{}-{}-{}".format(self.version, platform, arch)
+        source_url = "{}/dist/v{}/{}.{}".format(self.homepage, self.version, filename, extension)
+        tools.get(source_url)
+        extracted_dir = filename
+        os.rename(extracted_dir, self._build_subfolder)
 
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self._build_subfolder)
-        if self.settings.os_build == "Linux" and self.settings.arch_build == "x86":
-            with tools.chdir(self._source_subfolder):
-                autotools = self._configure_autotools()
-                autotools.install()
-                for package_subfolder in ["include", "lib", "share"]:
-                    shutil.rmtree(os.path.join(self.package_folder, package_subfolder))
-        else:
-            self.copy(pattern="*", src=self._build_subfolder, dst="", keep_path=True)
+        self.copy(pattern="*", src=self._build_subfolder, dst="", keep_path=True)
 
     def package_info(self):
         bin_dir = self.package_folder if tools.os_info.is_windows else os.path.join(self.package_folder, "bin")
